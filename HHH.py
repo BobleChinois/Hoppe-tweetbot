6bd30a90102ec220a32f875bd66f7869f4f7edd5
Sosthene <alexis.gaub@yahoo.fr>
2019-04-09 16:12:54 +0200

    Add tooting functionality

--- HHH.py
+++ HHH.py
@@ -3,21 +3,18 @@ import time
 import os
 from random import choice 
 import re
-
-APP_KEY = "AZGNmseKabNe9zy1Ud3bg0qtA"
-APP_SECRET = "6sZOdFQVZbHZtbRd1XMUVEDEKhzgXHw3vI5BibTyJOqRSPjW1P"
-OAUTH_TOKEN = "1110509646228480000-4YWPWBKLdwxIDqFPut86bUAWE1J7Xi"
-OAUTH_TOKEN_SECRET = "87jwWi9ITxfbr7t2T4qCG6r6uaVFtR7CayU33UO3gPwD8"
-
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-books_dir = os.path.join(os.path.dirname(__file__), 'books')
-memes_dir = os.path.join(os.path.dirname(__file__), 'memes')
+from HHH_masto import toot
+from importCredentials import importCredentials
 
 def parseText(text):
     # Separate the quote from the link
-    m = re.search(r'(?P<quote>.*)(?P<link>(https.*)?)', text)
+    tweet = {}
+    regex = r'(?P<quote>.*)?(?P<link>https.*)?'
+
+    m = re.search(regex, text)
+
     tweet = m.groupdict("")
-    # return a dict object with the quote and the link if any
+
     return tweet
 
 def matchPic(link):
@@ -37,21 +34,31 @@ def matchPic(link):
     else:
         return None
 
+cred_dir = os.path.join(os.path.dirname(__file__), 'twitter_id')
+books_dir = os.path.join(os.path.dirname(__file__), 'books')
+memes_dir = os.path.join(os.path.dirname(__file__), 'memes')
+
+creds = importCredentials(cred_dir) 
+twitter = Twython(*creds)
 
 try:
     with open('quotes', 'r+') as tweetfile:
         buff = tweetfile.readlines()
 
     for line in buff[:]:
+       
         line = line.strip(r'\n')
         text = parseText(line) 
         quote = '"' + text['quote'] + '"'
         link = text['link']
         tweet = quote + link
+
         link_length = len(link)
         tweet_length = len(tweet) - link_length + 23
-        if tweet_length <= 280 and len(line) > 0:
+
+        if tweet_length <= 280:
             print("Tweeting...")
+
             source = matchPic(link)
             
             if source != None:
@@ -65,18 +72,26 @@ try:
 
             print(f"{tweet}")
 
-            twitter.update_status(status=tweet, \
-                    media_ids=[response['media_id']])
+            #twitter.update_status(status=tweet, \
+            #        media_ids=[response['media_id']])
             
+            print("Tweeted!")
+            
+            toot(tweet, filename) 
+
+            print("Tooted!")
+
+            #with open('quotes', 'w') as tweetfile:
+            #    buff.remove(line)
+            #    tweetfile.writelines(buff)
+            time.sleep(21600)
+        else:
+            print("Skipped line - too long or non-existent")
             with open('quotes', 'w') as tweetfile:
                 buff.remove(line)
                 tweetfile.writelines(buff)
-            print("Tweeted!")
-            time.sleep(3600)
-        else:
-            print("Skipped line - too long or nothing")
-            print(line, 'is ', line_length, 'long')
             continue
+
     print("No more lines to tweet...")
 
 except TwythonError as e:
